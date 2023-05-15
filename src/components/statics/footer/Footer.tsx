24df47eba29625f4d07445cafdca8872a430ff7f
maurilosantos <maurilosantoss1@gmail.com>
2023-05-15 14:25:04 -0300

    alterando tamanho dos icons no footer

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -12,17 +12,17 @@ function Footer() {
                 <Grid alignItems="center" item xs={12}>
                     <Box style={{ backgroundColor: "#595b5a", height: "120px" }}>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h6" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.facebook.com/generationbrasil" target="_blank">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                                <FacebookIcon style={{ fontSize: 60, color: "white", width:"40px" }} />
                             </a>
                             <a href="https://www.instagram.com/generationbrasil/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                                <InstagramIcon style={{ fontSize: 60, color: "white", width:"40px"}} />
                             </a>
                             <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon style={{ fontSize: 60, color: "white", width:"40px" }} />
                             </a>
                         </Box>
                     </Box>
