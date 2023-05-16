cc8eb9402c35cf9eb729ee27daab5748060f96b4
maurilosantos <maurilosantoss1@gmail.com>
2023-05-16 12:08:42 -0300

    🔗 Criando rotas e página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import Navbar from './components/statics/navbar/Navbar';
 import Home from './paginas/home/Home';
 import Footer from './components/statics/footer/Footer';
 import './App.css'
+import Login from './paginas/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     < >
+      <BrowserRouter>
       <Navbar />
-      <Home />
+        <Routes>
+          <Route path='/' element = {<Login />}/>
+          <Route path='/login' element = {<Login />}/>
+          <Route path='/home' element = {<Home />}/>
+
+        </Routes>
       <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+import { Grid } from '@mui/material';
+
+function Login() {
+    return (
+        <>
+            <Grid container>
+                <Grid item xs={6}>  </Grid>
+                <Grid className='imageLogin' item xs={6}>  </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Login
