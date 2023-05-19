62f62c1196580cc69c1a503c82eae3be027f7931
maurilosantos <maurilosantoss1@gmail.com>
2023-05-19 12:07:47 -0300

    build: :sparkles: Criando componente de postagens e temas
    Criei dois componentes de postagens e temas chamando suas rotas no App.tsx pra quando o usuário logar ser possível acessar todas as suas postagens e temas.

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,30 @@ import './App.css'
 import Login from './paginas/login/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUruario';
+import ListaTema from './components/tema/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 
 function App() {
   return (
     < >
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <Routes>
-          <Route  path='/' element = {<Login />}/>
-          <Route path='/login' element = {<Login />}/>
-          <Route path='/home' element = {<Home />}/>
-          <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
-          
+          <Route path='/' element={<Login />} />
+
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/home' element={<Home />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/postagens" element={<ListaPostagem />} />
+
         </Routes>
-      <Footer />
+        <Footer />
       </BrowserRouter>
     </>
   );
